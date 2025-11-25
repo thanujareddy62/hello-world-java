1b051898411d5e72b7b41ac0912989a8fac43323
thanujareddy62 <119800469+thanujareddy62@users.noreply.github.com>
2025-11-25 15:15:13 +0530

    Add files via upload

---
+++ src/main/java/com/demo/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class HelloWorld {
+    public static void main(final String args[])
+    {
+        final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("h:mm:ss a 'on' MMM d, yyyy'.'");
+        final LocalDateTime now =  LocalDateTime.now();
+
+        System.out.println("Hello, World! The current time is "+dtf.format(now));
+    }
+}
